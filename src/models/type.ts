86acb9079bc7c04a07ef31198ea43ee2ee46d395
Rafael Manjua <seiinity@users.noreply.github.com>
2025-04-07 17:35:10 +0100

    Add type reference class

--- src/models/type.ts
+++ src/models/type.ts
@@ -1,5 +1,26 @@
-﻿export type Type =
+﻿import config from "../config";
+
+export type Type =
 {
     id?: number;
     name: string;
+}
+
+export class TypeReference
+{
+    name: string;
+    url?: string;
+
+    constructor(name: string, url?: string)
+    {
+        this.name = name;
+        this.url = url;
+    }
+
+    static build(types: Type[]): TypeReference[]
+    {
+        return types
+            .filter((type) => type.id && type.name)
+            .map((type) => new TypeReference(type.name!, `${config.baseUrl}/api/types/${type.id}`));
+    }
 }
