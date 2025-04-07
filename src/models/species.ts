f49b2f83bf4ea699c6e47c988c8eb2f13893c358
Rafael Manjua <seiinity@users.noreply.github.com>
2025-04-07 17:35:23 +0100

    Update species to use type references

--- src/models/species.ts
+++ src/models/species.ts
@@ -1,5 +1,14 @@
-﻿export type Species =
+﻿import { TypeReference } from "./type";
+
+export type Species =
 {
     id?: number;
     name: string;
+    types: TypeReference[];
+}
+
+export type SpeciesBody =
+{
+    name: string;
+    types: string[];
 }
